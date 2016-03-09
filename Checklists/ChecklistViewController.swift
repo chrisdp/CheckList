6f6f4ca7b214ddab06c5fde1dcfbf64610789c6a
chrisdp <chrisdwyerperkins@gmail.com>
2016-03-09 19:53:33 -0400

    Updated indentation.

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,53 +9,53 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 100
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
+    
+    let label = cell.viewWithTag(1000) as! UILabel
+    
+    switch indexPath.row % 5 {
+    case 0:
+      label.text = "Walk the dog"
+    case 1:
+      label.text = "Brush my teeth"
+    case 2:
+      label.text = "Learn iOS development"
+    case 3:
+      label.text = "Soccer practice"
+    case 4:
+      label.text = "Eat ice cream"
+    default:
+      break
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
-        
-        let label = cell.viewWithTag(1000) as! UILabel
-        
-        switch indexPath.row % 5 {
-        case 0:
-            label.text = "Walk the dog"
-        case 1:
-            label.text = "Brush my teeth"
-        case 2:
-            label.text = "Learn iOS development"
-        case 3:
-            label.text = "Soccer practice"
-        case 4:
-            label.text = "Eat ice cream"
-        default:
-            break
-        }
-        
-        return cell
+    return cell
+  }
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+      if cell.accessoryType == .None {
+        cell.accessoryType = .Checkmark
+      } else {
+        cell.accessoryType = .None
+      }
     }
-
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
-            if cell.accessoryType == .None {
-                cell.accessoryType = .Checkmark
-            } else {
-                cell.accessoryType = .None
-            }
-        }
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-    }
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+  }
 }
 
