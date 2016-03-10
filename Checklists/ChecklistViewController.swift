64964c8a561b2e2020c239bfff15d44b28ede588
chrisdp <chrisdwyerperkins@gmail.com>
2016-03-09 21:01:51 -0400

    Comments added where needed.

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ChecklistViewController: UITableViewController {
   
+  // label text values
   var rowtext = ["Walk the dog", "Brush my teeth", "Learn iOS development", "Soccer practice", "Eat ice cream"]
   
+  // completed/not completed state
   var rowchecked = [false, true, false, false, true]
   
   override func viewDidLoad() {
@@ -25,33 +27,40 @@ class ChecklistViewController: UITableViewController {
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // determine how many items can be show
     return rowtext.count
   }
   
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    // get cell and label reference
     let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
-    
     let label = cell.viewWithTag(1000) as! UILabel
     
+    // update label text
     label.text = rowtext[indexPath.row]
     
+    // update accessory type
     configureCheckmarkForCell(cell, indexPath: indexPath)
     return cell
   }
   
   func configureCheckmarkForCell(cell: UITableViewCell, indexPath: NSIndexPath) {
+    // check to see if checkmark should be displayed
     if (rowchecked[indexPath.row]) {
-        cell.accessoryType = .Checkmark
+      cell.accessoryType = .Checkmark
     } else {
       cell.accessoryType = .None
     }
   }
   
   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    // get cell reference
     if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+      // flip accessory indicator and update type
       rowchecked[indexPath.row] = !rowchecked[indexPath.row]
       configureCheckmarkForCell(cell, indexPath: indexPath)
     }
+    // display short fadeout rather than staying selected
     tableView.deselectRowAtIndexPath(indexPath, animated: true)
   }
 }
