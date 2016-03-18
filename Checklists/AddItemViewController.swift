c297e38e218a532db6aa488880f71b9ddce4a70b
chrisdp <chrisdwyerperkins@gmail.com>
2016-03-18 13:41:19 -0300

    Setup the AddItemController to be a delegate for the textField.

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -9,9 +9,10 @@
 import Foundation
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
   
   @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
   
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
@@ -31,6 +32,15 @@ class AddItemViewController: UITableViewController {
   
   override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
     return nil
+  }
+  
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    let oldText: NSString = textField.text!
+    let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+    
+    doneBarButton.enabled = newText.length > 0
+    
+    return true
   }
   
 }
