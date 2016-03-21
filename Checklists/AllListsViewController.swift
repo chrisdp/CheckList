ef1ab56327ef30bca8b4647bbca10804b5f1b180
chrisdp <chrisdwyerperkins@gmail.com>
2016-03-21 11:04:25 -0300

    Basic layout of AllItemsViewController with segue to ChecklistViewController.

---
+++ Checklists/AllListsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by christopher dwyer-perkins on 2016-03-21.
+//  Copyright © 2016 christopher dwyer-perkins. All rights reserved.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  // MARK: - Table view data source
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 3
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = cellForTableView(tableView)
+    cell.textLabel!.text = "List \(indexPath.row)"
+    return cell
+  }
+  
+  func cellForTableView(tableView: UITableView) -> UITableViewCell {
+    let cellIdentifier = "Cell"
+    if let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) {
+      return cell
+    } else {
+      return UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+    }
+  }
+  
+  // -------------------------------------------------- EVENT HANLDERS
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    performSegueWithIdentifier("ShowChecklist", sender: nil)
+  }
+
+}
